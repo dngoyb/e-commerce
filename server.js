9c84b83ed2699dbcc4b98ce01362a39dc80a4857
dngoyb <dngoyb@gmail.com>
2020-04-29 20:58:17 +0200

    css file added

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const router = require('./router/admin/auth');
 
 const app = express();
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
 	cookieSession({
